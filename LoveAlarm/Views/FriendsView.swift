d926b658bea1721838de37875de5f09014294cee
KT <ketongc@andrew.cmu.edu>
2020-08-28 21:31:22 -0400

    Added friends view

---
+++ LoveAlarm/Views/FriendsView.swift
@@ -0,0 +1,21 @@
+//
+//  FriendsView.swift
+//  LoveAlarm
+//
+//  Created by KT Chen on 8/28/20.
+//  Copyright © 2020 Xinda Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendsView()
+    }
+}
